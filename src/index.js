5db919e3daa811dddeba16013777bf6fb3832c9c
RobertoPotato <robertwabuti@gmail.com>
2021-01-11 20:12:52 +0300

    Devtools extension resumed working after removal of the first argument

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
-const composeEnhancer =
-  window.__REDUX_DEVTOOLS_EXTENSION__ /
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 
