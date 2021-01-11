e5699527eae3afd84ffad68d6ca529abba9e9769
RobertoPotato <robertwabuti@gmail.com>
2021-01-11 21:02:59 +0300

    Refactored the home page

--- src/actions/gamesActions.js
+++ src/actions/gamesActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { popularGamesUrl } from '../api';
+import { popularGamesUrl, upcomingGamesUrl, newGamesUrl } from '../api';
 
 //Action Creator
 //Add another arrow function, and add the dispatch in the second
@@ -7,11 +7,14 @@ import { popularGamesUrl } from '../api';
 export const loadGames = () => async (dispatch) => {
   //FETCH AXIOS
   const popularData = await axios.get(popularGamesUrl());
-
+  const newGamesData = await axios.get(newGamesUrl());
+  const upcomingData = await axios.get(upcomingGamesUrl());
   dispatch({
     type: 'FETCH_GAMES',
     payload: {
       popular: popularData.data.results,
+      upcoming: upcomingData.data.results,
+      newGames: newGamesData.data.results,
     },
   });
 };
